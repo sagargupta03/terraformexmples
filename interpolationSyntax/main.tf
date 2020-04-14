7268c980be107a48631c2d6a784f5c3ef5717952
Ubuntu <ubuntu@ip-172-31-88-187.ec2.internal>
2020-04-14 17:47:37 +0000

    interpolation practice

--- interpolationSyntax/main.tf
+++ interpolationSyntax/main.tf
@@ -1,12 +1,12 @@
 # Download the latest Ghost image
-resource "docker_image" "image_id" {
+resource "docker_image" "my_image2" {
   name = "ghost:latest"
 }
 
 # Start the Container
-resource "docker_container" "container_id" {
-  name  = "blog"
-  image = "${docker_image.image_id.latest}"
+resource "docker_container" "my_container2" {
+  name  = "My_container_blog_3"
+  image = "${docker_image.my_image2.latest}"
   ports {
     internal = "2368"
     external = "80"
